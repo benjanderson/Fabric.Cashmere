6c1ca0b0a3805e11d6d304b141f9f5c72784da12
Ben Anderson <ben.anderson@healthcatalyst.com>
2018-04-05 08:46:57 -0600

    WIP typeahead

--- docs/app/components/type-ahead/type-ahead-demo.component.ts
+++ docs/app/components/type-ahead/type-ahead-demo.component.ts
@@ -1,11 +1,64 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'hc-type-ahead-demo',
-  templateUrl: './type-ahead-demo.component.html',
-  styleUrls: ['./type-ahead-demo.component.scss']
+    selector: 'hc-type-ahead-demo',
+    templateUrl: './type-ahead-demo.component.html',
+    styleUrls: ['./type-ahead-demo.component.scss']
 })
 export class TypeAheadDemoComponent {
-    lastModified: Date = new Date( document.lastModified );
-    public document: string = require('raw-loader!../../../../guides/components/type-ahead.md');
+    lastModified: Date = new Date(document.lastModified);
+    public document: string = require('raw-loader!../../../../guides/components/type-ahead.md')
+    selected: string = '';
+    states: string[] = [
+        'Alabama',
+        'Alaska',
+        'Arizona',
+        'Arkansas',
+        'California',
+        'Colorado',
+        'Connecticut',
+        'Delaware',
+        'Florida',
+        'Georgia',
+        'Hawaii',
+        'Idaho',
+        'Illinois',
+        'Indiana',
+        'Iowa',
+        'Kansas',
+        'Kentucky',
+        'Louisiana',
+        'Maine',
+        'Maryland',
+        'Massachusetts',
+        'Michigan',
+        'Minnesota',
+        'Mississippi',
+        'Missouri',
+        'Montana',
+        'Nebraska',
+        'Nevada',
+        'New Hampshire',
+        'New Jersey',
+        'New Mexico',
+        'New York',
+        'North Dakota',
+        'North Carolina',
+        'Ohio',
+        'Oklahoma',
+        'Oregon',
+        'Pennsylvania',
+        'Rhode Island',
+        'South Carolina',
+        'South Dakota',
+        'Tennessee',
+        'Texas',
+        'Utah',
+        'Vermont',
+        'Virginia',
+        'Washington',
+        'West Virginia',
+        'Wisconsin',
+        'Wyoming'
+    ];
 }
